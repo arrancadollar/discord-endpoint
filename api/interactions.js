3d2ee167e235455b127eb95b7dd92e5bf18c4946
✪ niickz <glendaravennabaldwin4@hotmail.com>
2026-03-07 23:53:11 -0300

    Update interactions.js

--- api/interactions.js
+++ api/interactions.js
@@ -6,34 +6,33 @@ export const config = {
   },
 };
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).send("Method Not Allowed");
+async function buffer(readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
   }
+  return Buffer.concat(chunks);
+}
+
+export default async function handler(req, res) {
 
   const signature = req.headers["x-signature-ed25519"];
   const timestamp = req.headers["x-signature-timestamp"];
 
-  const chunks = [];
+  const rawBody = await buffer(req);
 
-  for await (const chunk of req) {
-    chunks.push(chunk);
-  }
-
-  const rawBody = Buffer.concat(chunks);
-
-  const isValid = verifyKey(
+  const isValidRequest = verifyKey(
     rawBody,
     signature,
     timestamp,
     process.env.DISCORD_PUBLIC_KEY
   );
 
-  if (!isValid) {
+  if (!isValidRequest) {
     return res.status(401).send("Invalid request signature");
   }
 
-  const body = JSON.parse(rawBody.toString());
+  const body = JSON.parse(rawBody.toString("utf8"));
 
   if (body.type === 1) {
     return res.status(200).json({ type: 1 });
